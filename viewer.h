ded222dfd7cd48ea42428f72a7bcfff977a73293
Zul <24516067+zulhailmie@users.noreply.github.com>
2021-03-20 23:44:40 +0800

    Tweaks to better fit font size 11 on the screen

--- viewer.h
+++ viewer.h
@@ -13,11 +13,11 @@
 #ifndef VIEWER_LINE_HEIGHT
 #define VIEWER_LINE_HEIGHT   13
 #endif
-#define VIEWER_Y_LIST        18
+#define VIEWER_Y_LIST        22         // (SCREENH/MAXLINES)+2
 #define VIEWER_NB_LINES      ((screen.h - VIEWER_Y_LIST - 1) / VIEWER_LINE_HEIGHT + 1)
 #define VIEWER_MARGIN        1
 #define VIEWER_X_STEP        32
-#define VIEWER_SIZE_MAX      16777216  // = 16 MB
+#define VIEWER_SIZE_MAX      16777216   // = 16 MB
 
 class CViewer : public CWindow
 {
